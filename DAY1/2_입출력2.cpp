0287c01e81fd1353699554fb05644f3d40722ef9
ecourse <smkang@codenuri.co.kr>
2025-03-10 10:37:18 +0900

    Add files via upload

--- DAY1/2_입출력2.cpp
+++ DAY1/2_입출력2.cpp
@@ -8,7 +8,7 @@ int main()
 
 	std::cout << std::hex << n << std::endl; // a, 즉, 16진수로 출력
 	
-	std::cout << n << std::endl; // 
+	std::cout << std::dec << n << std::endl; // 10, 다시 10진수로
 	
-	std::cout << n << std::endl; // 
+	std::cout << std::setw(10) << n << std::endl; // 10 자리에 맞추서 출력
 }
