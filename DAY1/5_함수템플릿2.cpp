5df8a657fd5bd8f6d6e0ff0e2376f69ed76b94b6
ecourse <smkang@codenuri.co.kr>
2025-03-10 15:00:19 +0900

    Add files via upload

---
+++ DAY1/5_함수템플릿2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+// #1. 인자 2개가 서로 다른 타입이 될수 있도록
+//     타입인자를 2개 받는 템플릿
+template<typename T1, typename T2>
+? Add(T1 a, T2 b)
+{
+	return a + b;
+}
+
+int main()
+{
+	// 타입을 명시적 전달 해도 되고 생략해도 됩니다.
+	std::cout << Add<int, double>(1, 2.2) << std::endl; 
+
+	std::cout << Add(1, 2.2) << std::endl;
+}
+
+
+
+
