841e04730edb9862989d01fa5c53ef503d26a72f
ecourse <smkang@codenuri.co.kr>
2025-03-10 10:58:38 +0900

    Add files via upload

--- DAY1/2_입출력3.cpp
+++ DAY1/2_입출력3.cpp
@@ -13,9 +13,14 @@ int main()
 	int n = 1;
 	double d = 3.4;
 	std::println("{}, {}", n, d);
-	std::println("{0}, {1}, {0}", n, d);
+	std::println("{0}, {1:10}, {0}", n, d);
+
+	// C#은 아래 처럼도 되는데, C++은 안됨
+//	WriteLine($"{n}, {d}");
 }
 // visual studio 2022 : 지원
 // g++				  : g++14 부터 지원
 
-// g++ 2_입출력3.cpp -std=c++23 -lstdexp 
+// g++ 2_입출력3.cpp -std=c++23 -lstdc++exp 
+
+// 입력은 "std::cin" 그대로 , 출력만 C++23에서 추가!
