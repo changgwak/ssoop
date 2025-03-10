67f2b3e6a6e7b574c28be4851f37d389f0fa2aed
ecourse <smkang@codenuri.co.kr>
2025-03-10 10:52:29 +0900

    Add files via upload

---
+++ DAY1/2_입출력3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// std::cout : C++ 초기 부터 사용된 출력을 위한 객체지만
+//			   비판이 아주 많았습니다
+// #1. 코드가 너무 장황스러워진다(길어진다.)
+// #2. 느리다.(printf 가 훨씬 빠름)
+
+// 그래서 C++23 에서 새로운 출력 방법이 등장 합니다.
+#include <print> // C++23 에서 추가된 헤더
+
+int main()
+{
+	int n = 1;
+	double d = 3.4;
+	std::println("{}, {}", n, d);
+	std::println("{0}, {1}, {0}", n, d);
+}
+// visual studio 2022 : 지원
+// g++				  : g++14 부터 지원
+
+// g++ 2_입출력3.cpp -std=c++23 -lstdexp 
