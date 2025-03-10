a73f192e032c39bbf83c26423bf56827986b78f8
ecourse <smkang@codenuri.co.kr>
2025-03-10 14:02:16 +0900

    Add files via upload

--- DAY1/4_함수2.cpp
+++ DAY1/4_함수2.cpp
@@ -1,7 +1,18 @@
 ﻿#include <cstdio>
 // 37page. overloading 
 
+// #1. 함수 오버로딩
+// => 동일한 이름의 함수를 여러개 만들수 있다.
+// => 단, 인자의 갯수나 타입이 달라서, 호출시 구별가능해야 한다.
+
+// #2. C#, Java, Swift 등의 대부분의 언어가 지원하지만
+//     C, Python 이 지원하지 않는 문법
+
 int square(int a)
+{
+	return a * a;
+}
+double square(double a)
 {
 	return a * a;
 }
