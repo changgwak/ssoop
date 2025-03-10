b859ae40ac33cfea0346f27c06b6d1d0de8f942a
ecourse <smkang@codenuri.co.kr>
2025-03-10 16:56:11 +0900

    Add files via upload

---
+++ DAY1/8_캐스팅1.cpp
@@ -0,0 +1,15 @@
+﻿// 10_캐스팅1.cpp    49 page
+#include <iostream>
+
+int main()
+{
+	int n = 3;
+
+	// #1. int변수 주소를 double* 에 담기
+//	double* p = &n; // error
+
+	double* p = (double*)&n; // ok. 
+
+	*p = 3.4; // 이순간의 메모리를 생각해 보세요
+
+}
