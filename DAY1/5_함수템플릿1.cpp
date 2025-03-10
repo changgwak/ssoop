45c17c34d54c40529aa878bf8cb390d1d8122cd3
ecourse <smkang@codenuri.co.kr>
2025-03-10 14:58:43 +0900

    Add files via upload

--- DAY1/5_함수템플릿1.cpp
+++ DAY1/5_함수템플릿1.cpp
@@ -14,11 +14,19 @@ T Add(T a, T b)
 {
 	return a + b;
 }
-
 int main()
 {
+	// #3. 아래 2줄은 아무 문제 없습니다.
 	std::cout << Add(1,   2 )  << std::endl;
 	std::cout << Add(1.1, 2.2) << std::endl;
+
+	// #4. 아래 코드를 생각해 보세요
+	std::cout << Add(1, 2.2) << std::endl; // error. 인자 2개 모두 T인데
+									// 인자가 int, double 로 전달하므로
+									// T 결정못함
+
+	// #5. 아래 처럼 사용하면 에러 아님
+	std::cout << Add<double>(1, 2.2) << std::endl;
 }
 
 
