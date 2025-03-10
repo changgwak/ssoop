78e9a8bd0cce58115ac1d30bea22eed8a5359828
ecourse <smkang@codenuri.co.kr>
2025-03-10 15:49:52 +0900

    Add files via upload

---
+++ DAY1/7_new_delete.cpp
@@ -0,0 +1,18 @@
+﻿// 메모리할당 - 47 page
+#include <iostream>
+
+int main()
+{
+	// C 스타일
+	int* p = (int*)malloc(sizeof(int) * 20);
+	free(p);
+
+	// C++ 스타일
+
+	int* p1 = new int; // int 한개, 즉 4바이트 할당
+	delete p1;		   // 메모리 제거
+
+	int* p2 = new int[10]; // int 10개
+	delete[] p2;		// 주의 배열할당한 경우 배열로 delete 사용
+}
+
