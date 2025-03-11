a747d9884b0a2347981da6d63a42189c71610413
ecourse <smkang@codenuri.co.kr>
2025-03-11 17:02:40 +0900

    Add files via upload

--- DAY2/9_초기화리스트4-1.cpp
+++ DAY2/9_초기화리스트4-1.cpp
@@ -1,13 +1,13 @@
 class Sample
 {
 	int n;
-
 	const int c;
 
 public:
-	Sample(int v)
+	Sample(int v) : n(0), c(v) // ?? ok. 초기화!
 	{
-		c = v; // ?? 될까요 ?
+//		c = v; // ?? 될까요 ?
+				// error. const 는 대입될수 없습니다.
 	}
 };
 
