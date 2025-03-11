0d911e766f40131f3ede5a378e97f494c6a212b1
ecourse <smkang@codenuri.co.kr>
2025-03-11 11:19:46 +0900

    Add files via upload

--- DAY2/4_OOP3.cpp
+++ DAY2/4_OOP3.cpp
@@ -4,6 +4,13 @@
 // => 타입을 만들때 상태를 나타내는 데이타와 상태를 가지고 연산을 수행하는 함수를
 //    묶어서 타입을 만들자
 
+// 함수를 구조체 안에 넣을때 장점
+// #1. 데이타를 함수 인자로 전달할 필요 없다.
+// #2. 다양한 구조체(클래스) 관련 문법을 통해서 사용하기 쉽고,
+//     안전한 타입을 설계 할수 있다. (오후에 배우는 내용)
+
+
+
 // C  구조체 : 구조체 안에 함수를 넣을수 없다.
 // C++구조체 : 구조체 안에 함수를 넣을수 있다.
 
@@ -17,12 +24,12 @@ struct Rect
 
 	// 멤버 함수( 다른 언어는 "메소드" 라고 표현)
 	// => 특징 : 함수 안에서 멤버 데이타에 직접 접근 할수 있다.
-	int getRectArea()
+	int getArea()
 	{
 		return (right - left) * (bottom - top);
 	}
 
-	void drawRect()
+	void draw()
 	{
 		std::cout << "draw rect" << std::endl;
 	}
@@ -32,11 +39,12 @@ int main()
 {
 	Rect rc = { 1,1, 10,10 };
 
-	int n1 = rc.getRectArea();
+	int n1 = rc.getArea(); // 객체지향 스타일
+							   // "rc 에게 면적좀 알려달라고 요청"
 
-	rc.drawRect();
+	rc.draw();
 
-//	int n1 = getRectArea(rc);
+//	int n1 = getRectArea(rc); // C 스타일 :함수에 데이타 전달
 //	drawRect(rc);
 
 }
