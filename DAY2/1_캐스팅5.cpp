60f6c5196b3f0fd5bc5512466a53e37add4daf5a
ecourse <smkang@codenuri.co.kr>
2025-03-11 09:07:58 +0900

    Add files via upload

--- DAY2/1_캐스팅5.cpp
+++ DAY2/1_캐스팅5.cpp
@@ -7,8 +7,21 @@ int main()
 	// c의 주소를 double* 에 담고 싶습니다.
 	double* p1 = (double*)&c; // ok. C 언어 스타일!!
 
+
 	// 위 코드를 C++ 스타일 캐스팅으로 해보세요
-	double* p2 = ? &c;
+//	double* p2 = ? &c;
+
+	double* p2 = static_cast<double*>(&c); // error
+	double* p2 = reinterpret_cast<double*>(&c); // error
+									// 상수성 제거 안됨
+	double* p2 = const_cast<double*>(&c); // error
+									// 동일 타입만 가능.
+
+	double* p2 = reinterpret_cast<double*>(const_cast<int*>(&c)); //ok
+				// const int* => int* 로 상수성 제거 후
+				// int* => double*    로 변경
+				
+
 
 }
 
