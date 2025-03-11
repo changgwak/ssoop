ae3d8c84af5823c27e6ee8146ed92db8d11bec2f
ecourse <smkang@codenuri.co.kr>
2025-03-11 11:00:14 +0900

    Add files via upload

---
+++ DAY2/정리.cpp
@@ -0,0 +1,26 @@
+// 정리.cpp
+
+// C++ 언어를 통해서 배워야 하는 것들
+
+// #1. 객체지향 문법은 아니지만 프로그래밍 관용구에서 C언어에는 없는
+//     C++ 만의 문법들
+// => namespace, auto, decltype, reference
+// => static_cast, reinterpret_cast, const_cast
+// => new, delete
+// => template, overloading, inline, default argument
+// => 후위 반환 타입
+// => {} 초기화
+// 1일차 ~ 2일차 오전까지 배운 것들
+
+
+// #2. 객체지향 프로그래밍 개념과 class 문법 7~8시간 정도
+
+
+// #3. 상속과 객체지향 디자인 - 4 ~ 5시간 정도
+
+
+// #4. C++ 표준 라이브러리(STL) 사용법 - 4~5시간 정도, 5일차
+
+
+// #5. 객체지향 문법은  아니지만 알아야 하는 문법들 - 5일차 3~4시간
+// => 연산자 재정의, 예외등.. 
