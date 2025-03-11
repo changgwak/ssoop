c34c4e1e75a869fc48675d26412f1a8dbeedd3cb
ecourse <smkang@codenuri.co.kr>
2025-03-11 14:54:06 +0900

    Add files via upload

--- DAY2/7_Vector7.cpp
+++ DAY2/7_Vector7.cpp
@@ -20,7 +20,32 @@ public:
 	~Vector() { delete[] ptr; }
 
 	int& at(int idx) { return ptr[idx]; }
+
+
+	void resize(int new_size)
+	{
+		if (new_size > sz)	// 커지는 경우
+		{
+			int* tmp = new int[new_size];
+
+			memcpy(tmp, ptr, sizeof(int) * sz);
+
+			delete[] ptr;	// ptr을 제거하는게 아니라
+							// ptr 이 가리키는 메모리 제거
+
+			ptr = tmp;
+
+			sz = new_size;
+
+		}
+		else
+		{
+			// 작아지는 경우
+		}
+	}
 };
+
+
 
 int main()
 {
