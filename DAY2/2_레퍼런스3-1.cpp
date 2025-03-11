6bd2b38ea3d2ad8b18aad1f03e2ddc5a816ee56f
ecourse <smkang@codenuri.co.kr>
2025-03-11 10:11:10 +0900

    Add files via upload

--- DAY2/2_레퍼런스3-1.cpp
+++ DAY2/2_레퍼런스3-1.cpp
@@ -8,8 +8,11 @@ void f1(int n)
 
 void f2(const int& n)
 {
-	int k = n;
+	// n 은 reference 지만 원리상 포인터 사용하는 경우가 대부분
+	int k = n; // 따라서 이코드는
+				// int k = *(n내부포인터) 이므로 접근시 오버헤드발생
 }
+
 int main()
 {
 	int x = 0;
