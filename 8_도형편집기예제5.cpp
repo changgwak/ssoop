b8657e23f404a87c79b924fd5da01afb20486396
ecourse <smkang@codenuri.co.kr>
2025-03-12 16:58:25 +0900

    Add files via upload

---
+++ 8_도형편집기예제5.cpp
@@ -0,0 +1,54 @@
+// 예제1     149 page
+#include <iostream>
+#include <vector>
+
+// #4. 모든 파생 클래스(도형)의 공통의 특징은 반드시 기반 클래스(Shape)
+//     에도 있어야 한다.
+// => 문법적인 규칙이 아닌 디자인 원칙
+// => 그래야 Shape* 로 해당 특징(draw) 를 사용할수 있다.
+
+
+// #5. 기반 클래스의 멤버 함수중에서 파생 클래스가 다시 만들게 되는 것은
+//     반드시 가상함수로 해야 한다.
+
+class Shape
+{
+	int color = 0;
+public:
+	virtual void draw() const { std::cout << "draw shape\n"; }
+};
+class Rect : public Shape
+{
+public:
+	virtual void draw() const { std::cout << "draw rect\n"; }
+};
+class Circle : public Shape
+{
+public:
+	virtual void draw() const { std::cout << "draw circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (1)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1) v.push_back(new Rect);
+		else if (cmd == 2) v.push_back(new Circle);
+
+		else if (cmd == 9)
+		{
+			for (auto s : v)
+			{
+				s->draw();
+			}
+		}
+	}
+}
+
+
+
