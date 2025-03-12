b4d5d46ad7ea76c5e743124cb9e557382671df3f
ecourse <smkang@codenuri.co.kr>
2025-03-12 14:02:12 +0900

    Add files via upload

---
+++ DAY3/5_상속1.cpp
@@ -0,0 +1,34 @@
+﻿// 5_상속1.cpp   132page ~
+#include <iostream>
+#include <string>
+
+// 상속
+// 개념 : 기존 타입을 확장해서 새로운 타입을 만드는 것
+
+// 표기법
+// C++    : class Student : public Person
+// C#     : class Student : Person
+// Python : class Student ( Person )
+// Java   : class Student extends Person
+
+class Person
+{
+	std::string name;
+	int age;
+};
+
+class Student : public Person
+{	
+	int    id;
+};
+
+class Professor : public Person
+{
+	int    major;
+};
+
+int main()
+{
+	Student s;
+	Professor p;
+}
