1508cfabc1d99d672ae1a05a8ef5ea17fd8dcff7
ecourse <smkang@codenuri.co.kr>
2025-03-12 10:11:30 +0900

    Add files via upload

--- DAY3/3.상수멤버함수5.cpp
+++ DAY3/3.상수멤버함수5.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 
+// 1. 멤버 함수를 만들때 "const member function" 으로 할지 말지 를 잘 판단하세요
+//    "상수 객체에 대해서도 호출 가능해야 하는가 ?"
+
+
 class Point
 {
 	int x, y;
@@ -9,16 +13,17 @@ public:
 
 	// 객체의 상태를 문자열로 반환하는 함수
 	// => java, C# 에서 널리 사용되는 기술
-	std::string to_string()
+	// => 아래 to_string() 은 const member function 이 되는것이 맞습니다.
+	std::string to_string() const 
 	{
-		std::string s = std::to_string(x) + " ," + std::to_string(y);
+		std::string s = std::to_string(x) + ", " + std::to_string(y);
 		return s;
 	}
 };
 
 int main()
 {
-	Point pt(1, 2);
+	const Point pt(1, 2);
 	std::cout << pt.to_string() << std::endl;
 	std::cout << pt.to_string() << std::endl;
 }
