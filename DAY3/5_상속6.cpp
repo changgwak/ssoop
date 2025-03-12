d22fbc5e6b1c83df9aba9d6e69b522dc53732016
ecourse <smkang@codenuri.co.kr>
2025-03-12 14:50:57 +0900

    Add files via upload

---
+++ DAY3/5_상속6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+class People
+{
+	std::string name;
+	int age;
+public:
+	People(const std::string& name, int age)
+		: name(name), age(age)
+	{
+	}
+};
+
+// 아래 주석대로 코드 작성해 보세요
+// 1. People 에서 파생된 Student 만드세요
+// 2. int id 멤버 데이타 추가하세요
+// 3. Student 생성자 만들어 보세요
+// 4. main 에서 Student 객체 생성해 보세요
+
+
+int main()
+{
+
+}
+
+
+
