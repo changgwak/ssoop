6157e3b4d56b2f5a5686727e3a6d7273af9ba896
ecourse <smkang@codenuri.co.kr>
2025-03-12 10:53:17 +0900

    Add files via upload

---
+++ DAY3/4_정적멤버1.cpp
@@ -0,0 +1,26 @@
+﻿// 3_정적멤버1 - 111 page
+#include <iostream>
+
+// 자동차 객체가 몇대나 생성되었는지 알고 싶다!!
+
+// 방법 #1. 
+
+class Car
+{
+	int color = 0;
+public:
+
+	int cnt = 0;
+
+	Car() { ++cnt; }
+	~Car() { --cnt; }
+};
+
+int main()
+{
+	Car c1;
+	Car c2;
+
+	std::cout << c1.cnt << std::endl;
+}
+
