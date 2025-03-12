65e3066f4ea2b0f57a4c4e45605ab95d9f6f4ace
ecourse <smkang@codenuri.co.kr>
2025-03-12 14:59:08 +0900

    Add files via upload

--- DAY3/5_상속6.cpp
+++ DAY3/5_상속6.cpp
@@ -18,9 +18,22 @@ public:
 // 3. Student 생성자 만들어 보세요
 // 4. main 에서 Student 객체 생성해 보세요
 
+class Student : public People
+{
+	int id;
+public:
+	// 아래 처럼 만들면 People 디폴트 생성자가 없으므로 에러 입니다.
+//	Student(int id) : id(id) {} // Student(int id) : id(id) { call People::People()} 
+
+	// 해결책  : 아래 처럼 People 의 생성자를 명시적으로 호출
+	Student(const std::string& name, int age, int id) 
+		: People(name, age), id(id) {}
+};
 
 int main()
 {
+	Student s1("kim", 30, 99);
+	Student s2("lee", 20, 88);
 
 }
 
