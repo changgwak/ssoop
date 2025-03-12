1ed9b56608fd1c219c032383c6becebf5bdcde15
ecourse <smkang@codenuri.co.kr>
2025-03-12 15:18:23 +0900

    Add files via upload

--- DAY3/6_Upcasting1.cpp
+++ DAY3/6_Upcasting1.cpp
@@ -37,8 +37,23 @@ int main()
 	// #3. Animal* 인 p3 를 가지고는 Animal 에서 파생된 멤버만 접근 가능
 	//     Dog 가 추가한 멤버는 접근 안됨
 
+	// C++ : static type check, 멤버 접근이 유효한가를 컴파일 시간에 확인
+	// python : dynamic type check, 실행시간에 확인
+
 	p3->age = 10;	// ok
 	p3->color = 10; // error
+
+
+	// #4. Animal* 인 p3로 Dog 고유 멤버 에 접근하려면
+	//     Dog* 타입으로 캐스팅해서 사용해야 한다.
+	// 즉, 개발자가 Dog 가 맞다고, 컴파일러에게 알려주는 것
+
+	static_cast<Dog*>(p3)->color = 10;// ok
+
+	// 단, 이경우 p3가 가리키는 곳이 Dog 가 아니라면
+	// 실행시간에 오류 나올수 있습니다.
+	// 개발자 책임!!
+	// "Dog 라는 확신" 이 있을때만 사용해야 합니다.
 }
 
 
