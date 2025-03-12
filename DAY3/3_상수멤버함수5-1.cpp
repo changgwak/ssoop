e23b5c741f9339c8a293a58b59af386886d7a8e4
ecourse <smkang@codenuri.co.kr>
2025-03-12 10:18:53 +0900

    Add files via upload

--- DAY3/3_상수멤버함수5-1.cpp
+++ DAY3/3_상수멤버함수5-1.cpp
@@ -2,12 +2,19 @@
 #include <string>
 
 
+// 논리적 상수성(logical constness)
+// => 외부 사용자 입장에서는 "상수 멤버 함수" 가 되는 것이 맞지만
+// => 클래스 멤버 함수 구현시 상수 멤버 함수 안에서 멤버 데이타 수정이 필요한 경우
+
+// 해결책 : mutable 키워드
+
 class Point
 {
 	int x, y;
 
-	std::string cache;
-	bool cache_valid = false;
+	// 상수 멤버 함수 안에서도 수정 가능하도록 하는 멤버 데이타 만들때 사용
+	mutable std::string cache;
+	mutable bool cache_valid = false;
 public:
 	Point(int a, int b) : x(a), y(b) {}
 
