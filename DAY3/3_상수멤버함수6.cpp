c2ec2e34c59ade6c3f294db1b72300e691033112
ecourse <smkang@codenuri.co.kr>
2025-03-12 10:26:59 +0900

    Add files via upload

--- DAY3/3_상수멤버함수6.cpp
+++ DAY3/3_상수멤버함수6.cpp
@@ -10,23 +10,25 @@ public:
 
 	// 상수 멤버 함수의 정확한 의미!
 
-	void set(int a, int b) 
+	void set(int a, int b)	// void set(Point* this, int a, int b)
 	{ 
 		x = a; 
 		y = b; 
 	}
-
-	void print() 
+//	void print()			// void print(Point* this)
+	void print() const		// void print(const Point* this)
 	{
+//		x = 100; // this->x = 100 인데
+			     // this 가 const Point* 이므로 변경안됨
+
 		std::cout << x << ", " << y << std::endl;
 	}
 };
 int main()
 {
 	const Point p(1, 2);
-
-	p.set(10, 20);	// error. 
-	p.print();		// error. 
+	p.set(10, 20);	// set(&p, 10, 20);  error
+	p.print();		// print(&p);
 
 }
 
