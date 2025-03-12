f137c2dbec8080dcc3e4c5514392f238ab8af886
ecourse <smkang@codenuri.co.kr>
2025-03-12 17:02:03 +0900

    Add files via upload

--- DAY3/8_도형편집기예제5.cpp
+++ DAY3/8_도형편집기예제5.cpp
@@ -8,23 +8,36 @@
 // => 그래야 Shape* 로 해당 특징(draw) 를 사용할수 있다.
 
 
+// #5. 기반 클래스의 멤버 함수중에서 파생 클래스가 다시 만들게 되는 것은
+//     반드시 가상함수로 해야 한다.
+
 class Shape
 {
 	int color = 0;
 public:
-	void draw() const { std::cout << "draw shape\n"; }
+	// 아래 함수는 virtual 로 할까요 ? non-virtual 할까요 ?
+	// => 파생 클래스가 override 할 이유 없습니다. non-virtual!!
+	void set_color(int c) { color = c; }
+
+	// 면적을 구하는 방법은 도형마다 다릅니다.
+	// => 파생 클래스가 override 해야 합니다.
+	// => virtual 이 되어야 합니다.
+	virtual int get_area() const { return -1; }
+
+	virtual void draw() const { std::cout << "draw shape\n"; }
 };
+
+
 
 class Rect : public Shape
 {
 public:
-	void draw() const { std::cout << "draw rect\n"; }
+	virtual void draw() const { std::cout << "draw rect\n"; }
 };
-
 class Circle : public Shape
 {
 public:
-	void draw() const { std::cout << "draw circle\n"; }
+	virtual void draw() const { std::cout << "draw circle\n"; }
 };
 
 int main()
