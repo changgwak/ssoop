ce36edbf6d37732468f8cddc3499c65b615965fb
ecourse <smkang@codenuri.co.kr>
2025-03-12 16:07:00 +0900

    Add files via upload

--- DAY3/7_가상함수1-1.cpp
+++ DAY3/7_가상함수1-1.cpp
@@ -9,6 +9,7 @@ public:
 	//				 포인터 타입으로 함수 결정
 	void Cry1() { std::cout << "Animal Cry1" << std::endl; } 
 
+
 	// virtual : dynamic binding 하라는 의미
 	//			 실행시간에 객체의 종류를 조사해서 호출 결정
 	virtual void Cry2() { std::cout << "Animal Cry2" << std::endl; } 
@@ -19,11 +20,8 @@ class Dog : public Animal
 public:
 	void Cry1() { std::cout << "Dog Cry1" << std::endl; }  
 	
-	
 	virtual void Cry2() { std::cout << "Dog Cry2" << std::endl; }
 };
-
-
 
 int main()
 {
@@ -32,7 +30,8 @@ int main()
 
 	Animal* p = &d;
 
-	p->Cry1(); 
+	p->Cry1();  // static binding
+	p->Cry2();  // dynamic binding
 }
 
 
