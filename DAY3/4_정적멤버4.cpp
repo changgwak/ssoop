8484edc81566fb1600298d2e49099a4eee23bd3b
ecourse <smkang@codenuri.co.kr>
2025-03-12 11:21:04 +0900

    Add files via upload

---
+++ DAY3/4_정적멤버4.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+// static member function
+
+class Car
+{
+	int color = 0;
+public:
+	static int cnt;
+
+	Car() { ++cnt; }
+	~Car() { --cnt; }
+};
+
+int Car::cnt = 0;
+
+
+int main()
+{
+
+	std::cout << Car::cnt << std::endl; // 0
+
+
+	Car c1;
+	Car c2;
+
+	std::cout << Car::cnt << std::endl;
+	std::cout << c1.cnt << std::endl;
+	std::cout << c2.cnt << std::endl;
+
+}
+
