e15633f810ff9578679fb44d5b1e002c2d9503a3
ecourse <smkang@codenuri.co.kr>
2025-03-12 13:50:21 +0900

    Add files via upload

--- DAY3/4_정적멤버용어.cpp
+++ DAY3/4_정적멤버용어.cpp
@@ -1,9 +1,15 @@
 // 4_정적멤버용어.cpp
 class Test
 {
-	int data1;
-	static int data2;
+						// C++							// java, C#
+	       int data1;	// non-static member data		instance field
+	static int data2;	// static member data			static   field
+
 public:
-	void f1() {}
-	static void f2() {}
+	void f1() {}		// non-static member function	instance method
+	static void f2() {}	// static member function		static   method
 };
+// rust
+// 일반 함수 : function
+// 멤버 함수 : method
+// static멤버함수 : associated function
