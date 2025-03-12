10f6467b87b754263f0ef5011af4fa527d0f230b
ecourse <smkang@codenuri.co.kr>
2025-03-12 16:46:31 +0900

    Add files via upload

--- DAY3/8_도형편집기예제4.cpp
+++ DAY3/8_도형편집기예제4.cpp
@@ -36,10 +36,16 @@ int main()
 
 		else if (cmd == 9)
 		{
-			// 해결책 #1. Rect* 또는 Circle* 로 캐스팅
+			// 해결책 #1. Shape* 인 s 를 Rect* 또는 Circle* 로 캐스팅
+			// => s가 가리키는 곳이 Rect 인지 Circle 인지 조사해야 한다.
+			// => 조사 방법은 내일 배우게 됩니다.
 			for (auto s : v)
 			{
-				static_cast<Rect*>(s)->draw();
+				if ( s가 Rect 를 가리키면 )
+					static_cast<Rect*>(s)->draw();
+				
+				else if (s가 Circle 를 가리키면)
+					static_cast<Circle*>(s)->draw();
 			}
 		}
 	}
