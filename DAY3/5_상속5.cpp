7037b8ee387a2b7c99dacf9a1e233ef39a699193
ecourse <smkang@codenuri.co.kr>
2025-03-12 14:48:40 +0900

    Add files via upload

---
+++ DAY3/5_상속5.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+class People
+{
+	std::string name;
+	int age;
+public:
+	// #1. 생성자가 한개도 없으면 컴파일러가 디폴트 생성자 제공
+
+	// #2. 되도록이면 생성자를 만들어서 멤버 데이타를 초기화 하세요
+	People(const std::string& name, int age)
+		: name(name), age(age)
+	{
+	}
+};
+
+int main()
+{
+//	People p; // 초기화 되지 않은 객체의 생성 - 좋은 코드 아닙니다.
+
+	People p("kim", 20); // 좋은 코드 
+}
+
+
+
