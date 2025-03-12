13be71ac73b6f0ce858cd1d056878525f58b88f6
ecourse <smkang@codenuri.co.kr>
2025-03-12 10:47:13 +0900

    Add files via upload

--- DAY3/explicit_object_parameter.cpp
+++ DAY3/explicit_object_parameter.cpp
@@ -12,20 +12,36 @@ public:
 
 	Point(int a, int b) : x(a), y(b) {}
 
-	void set(int a, int b) 
+
+	void set1(int a, int b) 
 	{ 
 		x = a; 
 		y = b; 
 	}
 
-	void print() const
+	// C++23 부터 this 가 암시적으로 추가되는 것 말고
+	// 아래 처럼 명시적으로 받을수도 있습니다.
+	// "explicit object parameter" 라는 문법
+	void set2(this Point& self, int a, int b)
+	{
+		self.x = a;
+		self.y = b;
+	}
+	void print1() const
 	{
 		std::cout << x << ", " << y << std::endl;
+	}
+	// 상수 멤버함수의 explicit object parameter는 아래 처럼
+	void print1(this const Point& self) 
+	{
+		std::cout << self.x << ", " << self.y << std::endl;
 	}
 };
 int main()
 {
 	Point p(1, 2);
-	p.set(10, 20);	
-	p.print();		
+	p.set1(10, 20);	
+	p.set2(10, 20);
+	p.print1();		
+	p.print2();
 }
