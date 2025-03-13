98f74f6b7e135dd19155ff8a38201c5f0dd9c1e0
ecourse <smkang@codenuri.co.kr>
2025-03-13 16:43:51 +0900

    Add files via upload

--- DAY4/typedef1.cpp
+++ DAY4/typedef1.cpp
@@ -1,10 +1,15 @@
 // typedef1.cpp
 
+// typedef A B    => B는 A의 별명이다.  라고 해석하지 마세요
+
 // 아래 코드 해석해 보세요
 typedef int DWORD;
 
+typedef int ARR[2];
 
 int main()
 {
-	DWORD n = 0; 
+	DWORD n = 0; // int n = 0
+
+	ARR a;
 }
