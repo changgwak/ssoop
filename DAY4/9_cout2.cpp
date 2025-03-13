48f73f4da2989fffcf623a8b028a24c98053d977
ecourse <smkang@codenuri.co.kr>
2025-03-13 14:39:46 +0900

    Add files via upload

--- DAY4/9_cout2.cpp
+++ DAY4/9_cout2.cpp
@@ -1,4 +1,18 @@
-#include <iostream>
+//#include <iostream>
+
+#include <cstdio>
+
+
+// 사용자가 std namespace 를 만들어서 추가하면 안되지만, 아래 코드는 수업을 위해서.
+namespace std
+{
+	class ostream
+	{
+	};
+
+	ostream cout;
+}
+
 
 int main()
 {
