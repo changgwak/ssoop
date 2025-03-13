52a3b340aff0b38836f86a7331f937ad22bcd478
ecourse <smkang@codenuri.co.kr>
2025-03-13 14:48:14 +0900

    Add files via upload

---
+++ DAY4/9_cout3.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+namespace std
+{
+	class ostream
+	{
+	public:
+		ostream& operator<<(int n)         { printf("%d", n);  return *this;}
+		ostream& operator<<(double d)      { printf("%lf", d); return *this;}
+		ostream& operator<<(const char* s) { printf("%s", s);  return *this;}
+	};
+	ostream cout;
+}
+int main()
+{
+	int    n = 10;
+	double d = 3.4;
+
+	std::cout << n << ", " << d;
+	// cout.operator<<(n).operator<<(", ").operator<<(d)
+
+}
+
+
