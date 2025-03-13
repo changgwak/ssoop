bae6620cbcf5377f4b95b4604088889dfeeef022
ecourse <smkang@codenuri.co.kr>
2025-03-14 08:44:06 +0900

    Add files via upload

---
+++ DAY5/1_예외3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// 던져진 예외 잡기 : try ~ catch
+
+int db_backup()
+{
+	if (1)		 
+		throw 1; 
+
+	return 100;
+}
+void db_remove() { std::cout << "remove db\n"; }
+
+int main()
+{
+	try
+	{
+		int ret = db_backup();
+
+		//....
+	}
+	catch (int e)
+	{
+		std::cout << "예외 발생\n";
+
+		// 예외의 원인을 해결할수 없다면 여기서 
+		// 사용자에게 알리고 종료 해야 합니다.
+		std::exit(-1);
+
+		// 종료 하지 않으면 프로그램을 계속 실행됩니다.
+	}
+
+
+
+	db_remove();
+}
+
