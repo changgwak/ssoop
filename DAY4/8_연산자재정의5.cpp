b2e6b4b06c42629058e1c06b597778003bd9b6f8
ecourse <smkang@codenuri.co.kr>
2025-03-13 14:05:46 +0900

    Add files via upload

--- DAY4/8_연산자재정의5.cpp
+++ DAY4/8_연산자재정의5.cpp
@@ -35,9 +35,17 @@ int main()
 	Point p3 = p1 + p2;	// error.
 						// 멤버 버전 또는 일반 함수 버전 중에서
 						// 한개만 제공해야 한다.
-
-
 	p3.print();
+
+	// 멤버 함수가 좋을까 ? non-member 함수가 좋을까 ?
+
+	// 멤버가 좋다는 주장 : private 데이타 접근하려면 멤버가 좋다.(캡슐화)
+	// non-member 주장 : 멤버로 안될때가 있다.
+
+	int n = 1;
+	Point p4 = p1 + p2;
+	Point p5 = p1 + n;
+	Point p6 = n + p1;
 }
 
 
