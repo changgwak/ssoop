00c20724de2c8bdda0b846366c68cb9c82e6fa35
ecourse <smkang@codenuri.co.kr>
2025-03-13 14:12:29 +0900

    Add files via upload

--- DAY4/8_연산자재정의5.cpp
+++ DAY4/8_연산자재정의5.cpp
@@ -42,10 +42,29 @@ int main()
 	// 멤버가 좋다는 주장 : private 데이타 접근하려면 멤버가 좋다.(캡슐화)
 	// non-member 주장 : 멤버로 안될때가 있다.
 
+	// 아래 3개를 멤버 + 로 만들때를 생각해 봅시다.
 	int n = 1;
-	Point p4 = p1 + p2;
-	Point p5 = p1 + n;
-	Point p6 = n + p1;
+	Point p4 = p1 + p2; // p1.operator+( Point ) 필요
+
+	Point p5 = p1 + n;	// p1.operator+( int )
+	
+	Point p6 = n + p1;	// n.operator+(Point) 인데.. 
+						// n 은 int 타입이므로 만들수 없다.
+						// 하지만 non-member 로는 만들수 있다.
+						// operator+(int, Point) 만들면 된다.
+
+	// 권장
+	// 교환법칙이 필요한 이항연산자
+	// +, -, * ... : non-member 권장
+
+	// 교환 법칙 필요 없고, 객체 상태가 변경되는 연산자
+	// => a += 1;
+	// +=, -=, ... : member 권장
+
+	// (), [], ->, = : member 만 가능 ( 167 ~ 168)
+	// 
+	// ?, ::, *, .* : 연산자 재정의 안됨
+		
 }
 
 
