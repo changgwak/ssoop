281a15359922b6048ffb9babb76a496702f3a73f
ecourse <smkang@codenuri.co.kr>
2025-03-13 15:09:38 +0900

    Add files via upload

---
+++ DAY4/9_cout5.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+// 171 page
+// 이 주제는 중요합니다. 알아 두세요
+
+// std::cout 으로 사용자 정의 타입을 출력하기
+
+class Point
+{
+	int x;
+	int y;
+public:
+	Point(int x = 0, int y = 0) : x(x), y(y) {}
+};
+
+int main()
+{
+	Point p(1, 2);
+
+	std::cout << p ; // std::cout.operator<<(Point) 가 있으면 됩니다.
+					 // 그런데, C++ 표준인 std::ostream 에 멤버함수를 추가할수 없습니다.
+					
+					// 해결책 : 연산자 재정의는 멤버가 아닌 일반 함수도 가능합니다.
+					// operator<<(std::cout, p)
+					// operator<<(std::ostream, Point) 가 있으면 됩니다.
+					
+					
+					
+}
+
+
