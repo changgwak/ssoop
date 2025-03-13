63f7a1537f72ee457dd1bb988060f1ce9faa1ef3
ecourse <smkang@codenuri.co.kr>
2025-03-13 13:54:51 +0900

    Add files via upload

--- DAY4/8_연산자재정의3.cpp
+++ DAY4/8_연산자재정의3.cpp
@@ -8,23 +8,26 @@ public:
 	Point(int x, int y) : x(x), y(y) {}
 
 	void print() const { std::cout << x << ", " << y << std::endl; }
+
+	friend Point operator+(const Point& p1, const Point& p2);
 };
 
 // 멤버가 아닌 함수로 만드는 operator+() 함수
-Point operator+(Point p1, Point p2)
+Point operator+(const Point& p1, const Point& p2)
 {
 	Point tmp(p1.x + p2.x, p1.y + p2.y);
 	return tmp;
 }
-
 
 int main()
 {
 	Point p1(1, 1);
 	Point p2(2, 2);
 
+	Point p3 = p1 + p2;	// operator+(p1, p2) 호출
+						// operator+(Point, Point)
 
-	Point p3 = p1 + p2;	
+	p3.print();
 }
 
 
