03a467df6c877e9558a6f2e6fa7129f1c6cb5918
ecourse <smkang@codenuri.co.kr>
2025-03-13 12:53:03 +0900

    Add files via upload

---
+++ DAY4/6_가상함수원리2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+// 가상함수를 사용하면 컴파일러가 아래와 같이 많은 코드를 추가 합니다.
+
+void* animal_vtable[] = { Animal_TYPE_INFO, &Animal::Cry, &Animal::Run  };
+
+class Animal
+{
+	void* vtptr = animal_vtable;  // <= 컴파일러가 추가한 멤버
+
+	int age;
+public:
+	virtual void Cry() {}
+	virtual void Run() {}
+};
+
+
+//--------------------
+void* dog_vtable[] = { Dog_TYPE_INFO, &Dog::Cry, &Animal::Run };
+
+class Dog : public Animal
+{
+	void* vtptr = dog_vtable;
+
+	int color;
+public:
+	void Cry() override {}
+};
+//-----------------------------------------------------
+int main()
+{
+	Animal a1, a2;
+	Dog    d;
+
+	Animal* p = &d;
+	p->Cry();  
+}
+
+
