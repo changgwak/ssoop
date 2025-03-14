04008d40ffc7ed0fba93a951e6d1e4c6af46edae
ecourse <smkang@codenuri.co.kr>
2025-03-14 10:58:15 +0900

    Add files via upload

--- DAY5/3_임시객체3.cpp
+++ DAY5/3_임시객체3.cpp
@@ -15,6 +15,14 @@ void draw_line(const Point& from, const Point& to) {}
 int main()
 {
 	// draw_line 함수로 (1, 1) ~ (5, 5) 에 선을 그려보세요
+
+	Point p1(1, 1);
+	Point p2(5, 5);
+
+	draw_line(p1, p2); 
+
+	// p1, p2 가 더이상 필요 없습니다. 즉시 파괴 되었을까요 ??
+
 }
 
 
