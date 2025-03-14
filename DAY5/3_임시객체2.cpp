fe818104a8bd7d56b3d2e0dab0f9e9774f623a3b
ecourse <smkang@codenuri.co.kr>
2025-03-14 10:54:23 +0900

    Add files via upload

---
+++ DAY5/3_임시객체2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+void draw_pixel(int x, int y) {}
+
+int main()
+{
+	// 1, 2 에 점을 그리고 싶다. 다음중 좋은 코드는 ?
+
+	// #1. 인자를 literal 로 전달
+	draw_pixel(1, 2);
+
+
+	// #2. 변수를 만들어서 전달
+	int x = 1, y = 2;
+	draw_pixel(x, y);
+}
+
+
+
+
+
