c366e4893629bb20558a001bf23905d40bbe0254
ecourse <smkang@codenuri.co.kr>
2025-03-14 09:44:37 +0900

    Add files via upload

--- DAY5/2_복사생성자1.cpp
+++ DAY5/2_복사생성자1.cpp
@@ -14,7 +14,7 @@ public:
 	// => 자신과 동일한 타입의 객체를 한개 받는 생성자
 	// => 사용자 만들지 않으면 컴파일러가 제공한다.
 	// => 컴파일러 제공 버전은 아래와 동일
-	Point(const Point& other) {}
+	Point(const Point& other) : x(other.x), y(other.y) {}
 };
 
 int main()
@@ -24,5 +24,6 @@ int main()
 //	Point p2(1);    // error. 인자가 한개인 생성자는 없다
 	Point p3(1,2);	// ok. 2번 생성자 호출
 
-	Point p4(p3);   
+	Point p4(p3);   // 복사 생성자 호출
+	Point p5 = p3;  // 복사 생성자 호출
 }
