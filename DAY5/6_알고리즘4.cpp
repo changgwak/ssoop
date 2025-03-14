c6eaec07bf85a1e64ae47935888c1978780330d0
ecourse <smkang@codenuri.co.kr>
2025-03-14 15:20:11 +0900

    Add files via upload

---
+++ DAY5/6_알고리즘4.cpp
@@ -0,0 +1,25 @@
+// 6_알고리즘1
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+	std::vector<int> v = { 1,2,9,4,3,6,7,3,9,10 };
+		
+	// 주어진 구간에서 3을 -1로 변경
+//	std::replace(v.begin(), v.end(), 3, -1);
+	 
+	// v에서 "짝수"를 0으로 변경해 보세요
+	// => 조건, 람다표현식으로 하세요
+	// ?
+
+	std::replace_if(v.begin(), v.end(), 
+		[](int n) { return n % 2 == 0; },
+		, 0);
+
+
+	for (auto e : v)
+		std::cout << e << ", ";
+}
