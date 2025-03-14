a666f170e608e303047fdf3a3241e70714f832e1
ecourse <smkang@codenuri.co.kr>
2025-03-14 14:39:28 +0900

    Add files via upload

--- DAY5/6_알고리즘1-1.cpp
+++ DAY5/6_알고리즘1-1.cpp
@@ -11,9 +11,20 @@ int main()
 
 	// std::find
 	// 검색 구간 : [first, last )
+	//			   last 는 검색 대상 아님. 반 개행구간(half-open range)	
 	// 반환 값   : 찾은곳을 가리키는 반복자
 	//            실패시 last 반환
 	auto ret1 = std::find(s.begin(), s.end(), 3);
+
+	if (ret1 == s.end())
+	{
+		std::cout << "검색 실패\n";
+	}
+	else
+	{
+		// 검색 성공. ret1 은 3을 가리키는 반복자(포인터라고 생각하세요)
+		std::cout << *ret1 << std::endl; // 3
+	}
 
 
 }
