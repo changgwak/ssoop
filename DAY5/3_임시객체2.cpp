bcf3a4579ecf232dab618befa2342c8bd91e1dc8
ecourse <smkang@codenuri.co.kr>
2025-03-14 10:55:46 +0900

    Add files via upload

--- DAY5/3_임시객체2.cpp
+++ DAY5/3_임시객체2.cpp
@@ -5,7 +5,8 @@ void draw_pixel(int x, int y) {}
 int main()
 {
 	// 1, 2 에 점을 그리고 싶다. 다음중 좋은 코드는 ?
-
+	// => 변수를 만들 필요 없습니다. 1번이 좋은 코드
+	// 
 	// #1. 인자를 literal 로 전달
 	draw_pixel(1, 2);
 
@@ -13,6 +14,8 @@ int main()
 	// #2. 변수를 만들어서 전달
 	int x = 1, y = 2;
 	draw_pixel(x, y);
+
+	//.....
 }
 
 
