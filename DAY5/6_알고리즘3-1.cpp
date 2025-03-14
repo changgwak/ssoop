73896bf13d9377caf2a93538d9d6009b326080e1
ecourse <smkang@codenuri.co.kr>
2025-03-14 15:12:50 +0900

    Add files via upload

--- DAY5/6_알고리즘3-1.cpp
+++ DAY5/6_알고리즘3-1.cpp
@@ -13,7 +13,16 @@ int main()
 
 	auto ret1 = std::find_if(v.begin(), v.end(), foo);
 
-	auto ret2 = std::find_if(v.begin(), v.end(),     );
+	// 람다 표현식(lambda expression)
+	// => C++11
+	// => 익명의 함수를 만드는 문법
+	// => 함수 이름을 전달하는 위치에 "함수 구현 자체"를 전달하는 기술
+
+	// [] : 람다표현식이 시작됨을 알리는 기호
+	//		lambda introducer
+
+	auto ret2 = std::find_if(v.begin(), v.end(),  
+					[](int n) { return n % 3 == 0; } );
 
 
 }
