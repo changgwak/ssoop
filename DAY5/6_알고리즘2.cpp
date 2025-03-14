de06a042760847256f48afa2ce59d1cd1b4d9baf
ecourse <smkang@codenuri.co.kr>
2025-03-14 15:01:43 +0900

    Add files via upload

--- DAY5/6_알고리즘2.cpp
+++ DAY5/6_알고리즘2.cpp
@@ -8,13 +8,13 @@
 // => 멤버 함수가 아닌 일반함수
 // => 대부분 std::알고리즘이름(시작반복자, 마지막다음반복자) 입니다.
 
-
 int main()
 {
 	std::vector<int> v = { 1,2,3,4,3,6,7,3,9,10 };
 
 	// 뒤집는 알고리즘은 std::reverse() 입니다.
-	// ?
+	
+//	std::reverse( v.begin(), v.end()  );
 
 
 	for (auto e : v)
@@ -25,6 +25,8 @@ int main()
 	// v 에서 3을 -1로 변경해 보세요.  
 	// ?
 	
+	std::replace( v.begin(), v.end(), 3, -1    );
+
 
 	for (auto e : v)
 		std::cout << e << ", ";
